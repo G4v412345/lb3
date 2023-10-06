bd8c38e67401cab4ca49ff58dcac85ab48684853
G4v412345 <131289952+G4v412345@users.noreply.github.com>
2023-10-06 08:36:29 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Xml.Linq;
+
+public class Log
+{
+    public List<Event> Events { get; set; } = new List<Event>(); 
+}
+public class Event
+{
+    public string date { get; set; } 
+    public string res { get; set; }
+    public string ipfrom { get; set; }
+    public string url_to { get; set; }
+    public string method { get; set; }
+    public int response { get; set; }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        string xml = @"<?xml version=""1.0"" ?>
+                        <log>
+                            <event date=""27/May/1999:02:32:46"" result=""success"">
+                                <ip-from>195.151.62.18</ip-from>
+                                <method>GET</method>
+                                <url-to>/misc/</url-to>
+                                <response>200</response>
+                            </event>
+                            <event date=""27/May/1999:02:41:47"" result=""success"">
+                                <ip-from>195.209.248.12</ip-from>
+                                <method>GET</method>
+                                <url-to>/soft.htm</url-to>
+                                <response>200</response>
+                            </event>
+                        </log>";
+        Log log = parsexml(xml);
+        foreach (var i in log.Events)
+        {
+            Console.WriteLine($"Date: {i.date}");
+            Console.WriteLine($"Result: {i.res}");
+            Console.WriteLine($"IP From: {i.ipfrom}");
+            Console.WriteLine($"Method: {i.method}");
+            Console.WriteLine($"URL To: {i.url_to}");
+            Console.WriteLine($"Response: {i.response}");
+            Console.WriteLine();
+        }
+    }
+    static Log parsexml(string xml)
+    { 
+        XElement root = XElement.Parse(xml);
+        Log log = new Log();
+        foreach (XElement eventElement in root.Elements("event"))
+        {
+            Event evt = new Event
+            {
+                date = eventElement.Attribute("date").Value,
+                res = eventElement.Attribute("result").Value,
+                ipfrom = eventElement.Element("ip-from").Value,
+                method = eventElement.Element("method").Value,
+                url_to = eventElement.Element("url-to").Value,
+                response = int.Parse(eventElement.Element("response").Value)
+            };
+
+            log.Events.Add(evt);
+        }
+
+        return log;
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+public class Note
+{
+    public int id { get; set; }
+    public string date { get; set; }
+    public string time { get; set; }
+    public string subject { get; set; }
+    public string text { get; set; }
+    public string telephone { get; set; }
+}
+
+
+public class Notepad
+{
+    public List<Note> Notes { get; set; } = new List<Note>();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string xml = @"<?xml version=""1.0"" ?>
+                        <notepad>
+                            <note id=""1"" date=""12/04/99"" time=""13:40"">
+                                <subject>Важлива ділова зустріч</subject>
+                                <importance />
+                                <text>
+                                    Треба зустрітися з Іваном Івановичем, попередньо зателефонувавши йому за телефоном <tel>123-12-12</tel>
+                                </text>
+                            </note>
+                            <note id=""2"" date=""12/04/99"" time=""13:58"">
+                                <subject>Зателефонувати додому</subject>
+                                <text>
+                                    <tel>124-13-13</tel>
+                                </text>
+                            </note>
+                        </notepad>";
+
+        Notepad notepad = parsexml(xml);
+
+        foreach (var note in notepad.Notes)
+        {
+            Console.WriteLine($"ID: {note.id}");
+            Console.WriteLine($"Date: {note.date}");
+            Console.WriteLine($"Time: {note.time}");
+            Console.WriteLine($"Subject: {note.subject}");
+            Console.WriteLine($"Text: {note.text}");
+            Console.WriteLine($"Telephone: {note.telephone}");
+            Console.WriteLine();
+        }
+    }
+
+    static Notepad parsexml(string xml)
+    {
+        XElement root = XElement.Parse(xml);
+        Notepad notepad = new Notepad();
+
+        foreach (XElement noteElement in root.Elements("note"))
+        {
+            Note note = new Note
+            {
+                id = int.Parse(noteElement.Attribute("id").Value),
+                date = noteElement.Attribute("date").Value,
+                time = noteElement.Attribute("time").Value,
+                subject = noteElement.Element("subject").Value,
+                text = noteElement.Element("text").Value
+            };
+
+            XElement telElement = noteElement.Element("text").Element("tel");
+            if (telElement != null)
+            {
+                note.telephone = telElement.Value;
+            }
+
+            notepad.Notes.Add(note);
+        }
+
+        return notepad;
+    }
+}
